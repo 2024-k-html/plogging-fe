bb9cc5c395f964cc0043ceae0b4522bcee5daedf
yiseoffline <lsy0476@naver.com>
2024-08-13 03:06:55 +0900

    feat: create eachPost Page

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/App` | `/(tabs)/explore` | `/(tabs)/pages/CameraScreen` | `/(tabs)/pages/Chat` | `/(tabs)/pages/Course` | `/(tabs)/pages/CourseMap` | `/(tabs)/pages/Gather` | `/(tabs)/pages/Home` | `/(tabs)/pages/Login` | `/(tabs)/pages/Map` | `/(tabs)/pages/Post` | `/(tabs)/pages/Quit` | `/(tabs)/pages/Ranking` | `/(tabs)/pages/SignUp` | `/(tabs)/pages/Write` | `/App` | `/_sitemap` | `/explore` | `/pages/CameraScreen` | `/pages/Chat` | `/pages/Course` | `/pages/CourseMap` | `/pages/Gather` | `/pages/Home` | `/pages/Login` | `/pages/Map` | `/pages/Post` | `/pages/Quit` | `/pages/Ranking` | `/pages/SignUp` | `/pages/Write`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/App` | `/(tabs)/explore` | `/(tabs)/pages/CameraScreen` | `/(tabs)/pages/Chat` | `/(tabs)/pages/Course` | `/(tabs)/pages/CourseMap` | `/(tabs)/pages/EachPost` | `/(tabs)/pages/Gather` | `/(tabs)/pages/Home` | `/(tabs)/pages/Login` | `/(tabs)/pages/Map` | `/(tabs)/pages/Post` | `/(tabs)/pages/Quit` | `/(tabs)/pages/Ranking` | `/(tabs)/pages/SignUp` | `/(tabs)/pages/Write` | `/App` | `/_sitemap` | `/explore` | `/pages/CameraScreen` | `/pages/Chat` | `/pages/Course` | `/pages/CourseMap` | `/pages/EachPost` | `/pages/Gather` | `/pages/Home` | `/pages/Login` | `/pages/Map` | `/pages/Post` | `/pages/Quit` | `/pages/Ranking` | `/pages/SignUp` | `/pages/Write`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- app/(tabs)/pages/EachPost.jsx
+++ app/(tabs)/pages/EachPost.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Text, View, SafeAreaView } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
-const EachPost = () => {
+const plogging_image = require("../../../assets/image/ploggingSignup.jpg");
+
+const EachPost = ({ navigation }) => {
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <Text>each post</Text>
+      <ScrollView>
+        <Image source={plogging_image} className="w-full h-56 resize-contain" />
+        <View className=" ml-4 ">
+          <Text className="text-2xl font-bold mt-4">제목</Text>
+          <Text className="mt-2 text-blue text-base">
+            시작 날짜 ~ 종료 날짜
+          </Text>
+          <View className="flex-row">
+            <Text className="mt-2 mr-10">n / 모집 인원</Text>
+            <Text className="mt-2">지역</Text>
+          </View>
+          <Text className="mt-4">내용</Text>
+          <View className="flex justify-center items-center mt-10">
+            <TouchableOpacity className="bg-green w-5/6 rounded-lg">
+              <Text className="text-center py-2 text-xl font-bold">
+                플로깅 참여하기
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-green w-5/6 mt-4 rounded-lg"
+              onPress={() => navigation.navigate("gather")}
+            >
+              <Text className="text-center py-2 text-xl font-bold">
+                뒤로 가기
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
