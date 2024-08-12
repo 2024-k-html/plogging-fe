98a0744074e9b0f7ccb812015d548e0ffd761a4f
yiseoffline <lsy0476@naver.com>
2024-08-12 15:04:50 +0900

    feat: create map page

--- app/(tabs)/App.jsx
+++ app/(tabs)/App.jsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./pages/Home";
 import Ranking from "./pages/Ranking";
 import Gather from "./pages/Gather";
+import Map from "./pages/Map";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
       <Stack.Screen name="home" component={Home} />
       <Stack.Screen name="ranking" component={Ranking} />
       <Stack.Screen name="gather" component={Gather} />
+      <Stack.Screen name="map" component={Map} />
     </Stack.Navigator>
   );
 };
